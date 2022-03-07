a8e41afed36d01fdfba64be559cec7646a371fe2
AndrewIndeche <indecheandrew@gmail.com>
2022-03-07 07:44:47 +0300

    add head icon

---
+++ components/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Topnavbar from './Topnavbar';
+
+const home = (props) => {
+  return(
+    <div>
+    <container>
+    </container>
+    </div>
+  )
+}
---
+++ components/topnavbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Topnavbar = (props) =>{
+  return(
+    <container>
+      <content>
+        <Logo>
+        <a href="/home">
+             <img src="/images/netflix.png" alt="" />
+           </a>
+        </Logo>
+      </content>
+    </container>
+  )
+}
