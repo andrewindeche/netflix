3e7bb091553d667b9081b75768c249dad5ded1a0
AndrewIndeche <indecheandrew@gmail.com>
2022-03-12 19:08:11 +0300

    add login page logo

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,23 +2,32 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Login = (props)=> {
-    return(
+  return(
         <>
       <Img>
-        <img src ='/images/netflix.png' alt=""/>
+        <img src ='/images/netflixxx.jpeg' alt=""/>
         </Img>
         <Container>
-        <h1>Unlimited movies,TV shows, and more.</h1>
-        <h3>Watch anywhere.Cancel anytime.</h3>
-        <h4>Ready to watch?Enter your email to create or restart your membership.</h4>
+        <h1>Unlimited movies,TV</h1>
+        <h1>shows, and more.</h1>
+        <h5>Watch anywhere.Cancel anytime.</h5>
+        <h5>Ready to watch?Enter your email to create or restart your membership.</h5>
         </Container>
+        <BgImage/>
         </>
       );
     }
 const Img = styled.div`
-width:10%;
+width: 10px;
+height: auto;
 `;
 const Container = styled.div`
-margin:14em;
+text-align: center;
+padding-top:5em;
 `;
+const BgImage  = styled.div`
+background-image:url("/images/bg.jpg");
+position: absolute;
+`;
+
 export default Login;
