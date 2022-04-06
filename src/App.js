255ab16f5621d3d58c947c1d9a5b31f2086e0109
AndrewIndeche <indecheandrew@gmail.com>
2022-04-06 11:43:21 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 import './index.css';
 import './App.css';
 import LandingPage from './components/LandingPage';
@@ -9,18 +9,28 @@ import Infoplacard3 from './components/Infoplacard3';
 import Faqs from './components/Faqs';
 import Bottomnav from './components/Bottomnav';
 import Home from './components/Home';
+import Nav from './components/Nav';
 
 function App() {
   return (
       <>
     <div className="App">
-      <LandingPage/>
-      <Infoplacard/>
-      <Infoplacard2/>
-      <Infoplacard3/>
-      <Faqs/>
-      <Bottomnav/>
-      <Home/>
+      <Router>
+        <Nav/>
+          <Routes>
+            <Route exact path="/">
+             <LandingPage/>
+              <Infoplacard/>
+              <Infoplacard2/>
+              <Infoplacard3/>
+              <Faqs/>
+              <Bottomnav/>
+              </Route>
+              <Route exact path="/home">
+              <Home/>
+              </Route>
+        </Routes>
+      </Router>
       </div>
       </>
         );
