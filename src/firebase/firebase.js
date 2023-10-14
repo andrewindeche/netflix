2806f49802f50b36acaf779fa9e265f5a8b2ad45
AndrewIndeche <indecheandrew@outlook.com>
2023-10-15 00:00:29 +0300

    update sign in config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/compat/firestore';
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCU7xAqotkbyumGhjpmXS84e48KrRIijAw",
-  authDomain: "netflixclone-ad7b1.firebaseapp.com",
-  projectId: "netflixclone-ad7b1",
-  storageBucket: "netflixclone-ad7b1.appspot.com",
-  messagingSenderId: "868889073073",
-  appId: "1:868889073073:web:52c40e8d285410c0291990",
-  measurementId: "G-MTM4LQR8KT"
+  apiKey: "AIzaSyBM4_MbUa9_kAIpzkUSDJH-46BljTHORFQ",
+  authDomain: "guileless-concha-10401f.firebaseapp.com",
+  projectId: "guileless-concha-10401f",
+  storageBucket: "guileless-concha-10401f.appspot.com",
+  messagingSenderId: "282519930794",
+  appId: "1:282519930794:web:ec19124726ccff3bbc0dd3",
+  measurementId: "G-0EMFJ3T9B6"
 };
 
 // Initialize Firebase
