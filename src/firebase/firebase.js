187cf8fcfb408d9fea9a8d47b96d67e288169de0
Andrew Indeche <115837859+andrewindeche@users.noreply.github.com>
2024-09-27 08:26:53 +0300

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,31 @@ import 'firebase/compat/firestore';
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBM4_MbUa9_kAIpzkUSDJH-46BljTHORFQ",
-  authDomain: "guileless-concha-10401f.firebaseapp.com",
-  projectId: "guileless-concha-10401f",
-  storageBucket: "guileless-concha-10401f.appspot.com",
-  messagingSenderId: "282519930794",
-  appId: "1:282519930794:web:ec19124726ccff3bbc0dd3",
-  measurementId: "G-0EMFJ3T9B6"
+  import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = getStorage(firebaseApp);
+
+export {auth, provider,storage};
+export default db;
+
 };
 
 // Initialize Firebase
